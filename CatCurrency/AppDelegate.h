13ea48a2a454f922b8cd49e4a4c72716471038c2
Mohamad Taufiq <tfqnet@gmail.com>
2016-12-25 21:06:57 +0800

    branch dev

--- CatCurrency/AppDelegate.h
+++ CatCurrency/AppDelegate.h
@@ -5,7 +5,7 @@
 //  Created by PETRONAS ICT SDN BHD on 25/12/2016.
 //  Copyright © 2016 TfqNet. All rights reserved.
 //
-//app
+//app dev
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
